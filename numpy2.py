963bbc6f97a1b8836994edbd1e0651a730bcb09a
rtorresq2023 <140999079+rtorresq2023@users.noreply.github.com>
2023-08-08 21:52:24 -0500

    Add files via upload

---
+++ numpy2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  7 20:43:47 2023
+
+@author: usuario
+"""
+
+import numpy as np
+a=np.array([(1,2,3,4),
+            (3,4,5,6)])
+print(a)
+print("\n"*1)
+print(a[1,2])
